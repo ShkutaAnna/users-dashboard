fe03d0b33bde5d065738e54d46406d97d2aa5aa2
Anna <shkutaanna@gmail.com>
2026-08-06 21:26:27 +0300

    fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ import ChevRightIcon from "../assets/icons/chev-right.svg?react";
 import ChevLeftIcon from "../assets/icons/chev-left.svg?react";
 
 export default function Sidebar() {
-  const navigate = useNavigate(); // test
+  const navigate = useNavigate();
   const [collapsed, setCollapsed] = useState(true);
   
   return (
